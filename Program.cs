dc436498688bef55b78ee111114765b954a0b694
camillofalcao <camillofalcao@gmail.com>
2021-05-24 21:52:48 -0300

    Adicionado um comentário para testes

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace OlaMundo
     {
         static void Main(string[] args)
         {
+            //Comentário para teste
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Tecle Enter para finalizar.");
             Console.ReadKey();
