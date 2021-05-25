a69f768c9e3221bd3b309dbdb06b7181b533124a
camillofalcao <camillofalcao@gmail.com>
2021-05-24 21:46:22 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Tecle Enter para finalizar.");
+            Console.ReadKey();
+        }
+    }
+}
